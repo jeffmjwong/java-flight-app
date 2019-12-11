e646d4e028e5079abbd4d2a412c5147e58767305
Jeff Wong <v-jwong1@expedia.com>
2019-12-11 16:41:10 +1000

    create methods for Flight class

--- src/com/pluralsight/flight/Flight.java
+++ src/com/pluralsight/flight/Flight.java
@@ -1,4 +1,23 @@
 package com.pluralsight.flight;
 
 public class Flight {
+    int seats;
+    int passengers;
+
+    Flight() {
+        seats = 150;
+        passengers = 0;
+    }
+
+    public void add1Passenger() {
+        if (passengers < seats) {
+            passengers++;
+        } else {
+            handleTooMany();
+        }
+    }
+
+    private void handleTooMany() {
+        System.out.println("Too many passengers!");
+    }
 }

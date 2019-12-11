9bbf5cc72df2d08873be77cdef862b91223d136a
Jeff Wong <v-jwong1@expedia.com>
2019-12-11 16:43:15 +1000

    initialize an instance of Flight

--- src/com/pluralsight/flight/Main.java
+++ src/com/pluralsight/flight/Main.java
@@ -3,6 +3,13 @@ package com.pluralsight.flight;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Flight flight1 = new Flight();
+
+        flight1.add1Passenger();
+        flight1.add1Passenger();
+        flight1.add1Passenger();
+
+	    System.out.println(flight1.passengers);
+        System.out.println(flight1.seats);
     }
 }

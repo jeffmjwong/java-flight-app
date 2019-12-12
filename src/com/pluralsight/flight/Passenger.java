5d4c3c46f4e85db5d3f11eed6a75dbb1d907d081
Jeff Wong <v-jwong1@expedia.com>
2019-12-12 19:08:46 +1000

    private constructor

--- src/com/pluralsight/flight/Passenger.java
+++ src/com/pluralsight/flight/Passenger.java
@@ -13,11 +13,16 @@ public class Passenger {
     public Passenger() {}
 
     public Passenger(int freeBags) {
+        this(freeBags > 1 ? 25.0d : 50.0d);
         this.freeBags = freeBags;
     }
 
     public Passenger(int freeBags, int checkedBags) {
         this(freeBags);
         this.checkedBags = checkedBags;
+    }
+
+    private Passenger(double perBagFee) {
+        this.perBagFee = perBagFee;
     }
 }

35d0f84b027e2909d9376407097e56de01a03d94
Jeff Wong <v-jwong1@expedia.com>
2019-12-15 17:45:44 +1000

    create CargoOnlyPilot class

--- src/com/pluralsight/flight/Pilot.java
+++ src/com/pluralsight/flight/Pilot.java
@@ -1,4 +1,19 @@
 package com.pluralsight.flight;
 
-public class Pilot {
+public abstract class Pilot {
+    private Flight currentFlight;
+
+    public void fly(Flight flight) {
+        if (canAccept(flight)) {
+            this.currentFlight = flight;
+        } else {
+            this.handleCantAccept();
+        }
+    }
+
+    public abstract boolean canAccept(Flight flight);
+
+    private void handleCantAccept() {
+        System.out.println("Can't accept the flight!!");
+    }
 }

9abcd250341a5d9daee54172318a0cfd5ccd6beb
Jeff Wong <v-jwong1@expedia.com>
2019-12-15 14:47:43 +1000

    object equality

--- src/com/pluralsight/flight/Main.java
+++ src/com/pluralsight/flight/Main.java
@@ -3,19 +3,10 @@ package com.pluralsight.flight;
 public class Main {
 
     public static void main(String[] args) {
-	    Flight f1 = new Flight();
-	    Passenger p1 = new Passenger(0, 1);
-	    Passenger p2 = new Passenger(0, 2);
+    	Flight f1 = new Flight();
+    	Flight f2 = new Flight();
+    	Flight f3 = f2;
 
-		f1.addPassengers(p1, p2);
-
-		System.out.println(f1.getPassengers());
-		System.out.println(f1.getMaxCarryOns());
-		System.out.println(f1.getTotalCarryOns());
-		System.out.println(f1.getTotalCheckedBags());
-
-		CargoFlight cf1 = new CargoFlight();
-		cf1.add1Package(5.0f, 3.5f, 8.5f);
-		System.out.println(cf1.getUsedCargoSpace());
+    	System.out.println(f2 == f3);
     }
 }

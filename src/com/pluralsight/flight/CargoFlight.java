80d16876f20a808f85a1cd1d52de6d77d4133de4
Jeff Wong <v-jwong1@expedia.com>
2019-12-15 13:42:46 +1000

    basic methods for CargoFlight class

--- src/com/pluralsight/flight/CargoFlight.java
+++ src/com/pluralsight/flight/CargoFlight.java
@@ -1,4 +1,10 @@
 package com.pluralsight.flight;
 
-public class CargoFlight {
+public class CargoFlight extends Flight {
+    float maxCargoSpace = 1000.0f;
+    float usedCargoSpace;
+
+    private boolean hasCargoSpace(float size) {
+        return this.usedCargoSpace + size <= this.maxCargoSpace;
+    }
 }

62dbab1779a5961bd37bbc3d4a459c1c6cd58a6c
Jeff Wong <v-jwong1@expedia.com>
2019-12-15 18:03:53 +1000

    create FullLicensePilot class

--- src/com/pluralsight/flight/CargoOnlyPilot.java
+++ src/com/pluralsight/flight/CargoOnlyPilot.java
@@ -1,4 +1,8 @@
 package com.pluralsight.flight;
 
-public class CargoOnlyPilot {
+public class CargoOnlyPilot extends Pilot {
+    @Override
+    public boolean canAccept(Flight flight) {
+        return flight.getPassengers() == 0;
+    }
 }

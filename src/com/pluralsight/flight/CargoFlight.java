07604cdc0c2fd10a5e407504bdcab8965c2b955a
Jeff Wong <v-jwong1@expedia.com>
2019-12-15 16:05:52 +1000

    mark add1Package method as final

--- src/com/pluralsight/flight/CargoFlight.java
+++ src/com/pluralsight/flight/CargoFlight.java
@@ -6,7 +6,7 @@ public class CargoFlight extends Flight {
 
     public float getUsedCargoSpace() { return this.usedCargoSpace; }
 
-    public void add1Package(float h, float w, float d) {
+    public final void add1Package(float h, float w, float d) {
         float size = h * w * d;
 
         if (this.hasCargoSpace(size)) {

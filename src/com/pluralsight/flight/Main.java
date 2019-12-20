7e6c6a7c79c86b0dbfa59c8d7c6ad7a3fe1a1a6a
Jeff Wong <v-jwong1@expedia.com>
2019-12-20 15:14:58 +1000

    use StringBuilder to create string

--- src/com/pluralsight/flight/Main.java
+++ src/com/pluralsight/flight/Main.java
@@ -4,10 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
     	Flight f1 = new Flight(175);
-    	Flight f2 = new Flight('Z');
-    	CargoFlight cf1 = new CargoFlight(200);
+    	String location = "Florida";
+    	StringBuilder sb = new StringBuilder(40);
 
-    	System.out.println("My flight is " + f1);
-    	System.out.println("My flight is " + f2);
+    	sb.append("I flew to ");
+    	sb.append(location);
+    	sb.append(" on ");
+    	sb.append(f1.toString());
+
+    	int time = 9;
+    	int pos = sb.length() - " on ".length() - f1.toString().length();
+
+    	sb.insert(pos, " at ");
+    	sb.insert(pos + 4, time);
+
+    	String message = sb.toString();
+    	System.out.println(message);
     }
 }

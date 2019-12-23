3f1551a73d8261574c6ce1a4c92400a5a5c3aa3a
Jeff Wong <v-jwong1@expedia.com>
2019-12-23 18:07:48 +1000

    handle more files and io related exceptions

--- src/com/pluralsight/flight/Main.java
+++ src/com/pluralsight/flight/Main.java
@@ -1,9 +1,6 @@
 package com.pluralsight.flight;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.InputStream;
+import java.io.*;
 
 public class Main {
 
@@ -22,12 +19,12 @@ public class Main {
 
 			System.out.println("Total " + total);
     	} catch (NumberFormatException e) {
-			System.out.println("NumberFormatException occurs!");
-			System.out.println(e.getMessage());
-		} catch (Exception e) {
-			System.out.println("Lol some standard errors");
-			System.out.println(e.getMessage());
-    	} finally {
+			System.out.println("NumberFormatException occurs! Error: " + e.getMessage());
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found! Error: " + e.getMessage());
+    	} catch (IOException e) {
+			System.out.println("Some random IO exception! Error: " + e.getMessage());
+		} finally {
     		try {
 				if (reader != null) {
 					reader.close();

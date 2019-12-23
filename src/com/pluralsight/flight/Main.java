9b30f21e8bffe9c93c57cd7eda1878a28e0fc8e2
Jeff Wong <v-jwong1@expedia.com>
2019-12-23 14:34:49 +1000

    try catch block

--- src/com/pluralsight/flight/Main.java
+++ src/com/pluralsight/flight/Main.java
@@ -5,5 +5,15 @@ public class Main {
     public static void main(String[] args) {
 		Flight f1 = new Flight('s');
 		System.out.println("My flight is " + f1.toString());
+
+		int i = 12;
+		int j = 2;
+
+		try {
+			int k = i / (j - 2);
+			System.out.println(k);
+		} catch (Exception e) {
+			System.out.println("Error! " + e.getMessage());
+		}
 	}
 }

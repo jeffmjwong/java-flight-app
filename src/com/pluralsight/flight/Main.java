e75378715b3f5b7f5c43dd1e4ad969d56d51dfe9
Jeff Wong <v-jwong1@expedia.com>
2019-12-26 16:10:39 +1000

    specify FlightIterator as inner class instead of standalone class

--- src/com/pluralsight/flight/Main.java
+++ src/com/pluralsight/flight/Main.java
@@ -3,13 +3,25 @@ package com.pluralsight.flight;
 public class Main {
 
     public static void main(String[] args) {
-    	Passenger steve = new Passenger();
+    	Passenger steve = new Passenger("steve");
     	steve.getRewardProgram().setMemberLevel(3);
     	steve.getRewardProgram().setMemberDays(180);
     	System.out.println(steve.getRewardProgram().getMemberLevel());
     	System.out.println(steve.getRewardProgram().getMemberDays());
 
     	Passenger.RewardProgram platinum = new Passenger.RewardProgram();
-    	System.out.println(platinum.getMemberLevel());
+    	platinum.setMemberLevel(3);
+
+    	if (steve.getRewardProgram().getMemberLevel() == platinum.getMemberLevel()) {
+    		System.out.println("Steve is platinum member!");
+		}
+
+    	Flight lax045 = new Flight(45);
+    	lax045.setRoster(new Passenger[] { steve });
+    	lax045.setCrew(new CrewMember[] {});
+
+    	for (Person p : lax045) {
+    		System.out.println(p.getName());
+		}
 	}
 }

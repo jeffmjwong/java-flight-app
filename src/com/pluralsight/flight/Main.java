4c9374075fb4b28385c3b66f6f85b0511dc392af
Jeff Wong <v-jwong1@expedia.com>
2019-12-26 10:47:45 +1000

    implements Comparable interface on Passenger class

--- src/com/pluralsight/flight/Main.java
+++ src/com/pluralsight/flight/Main.java
@@ -1,11 +1,31 @@
 package com.pluralsight.flight;
 
-import java.io.*;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 		Flight f1 = new Flight('s');
 		System.out.println("My flight is " + f1.toString());
+
+		Passenger bob = new Passenger();
+		bob.setLevelAndDays(1, 180, "bob");
+		Passenger jane = new Passenger();
+		jane.setLevelAndDays(1, 90, "jane");
+		Passenger steve = new Passenger();
+		steve.setLevelAndDays(2, 180, "steve");
+		Passenger lisa = new Passenger();
+		lisa.setLevelAndDays(3, 730, "lisa");
+
+		Passenger[] passengers = { bob, jane, steve, lisa };
+		System.out.println(passengers[0].name);
+		System.out.println(passengers[1].name);
+		System.out.println(passengers[2].name);
+		System.out.println(passengers[3].name);
+		Arrays.sort(passengers);
+		System.out.println(passengers[0].name);
+		System.out.println(passengers[1].name);
+		System.out.println(passengers[2].name);
+		System.out.println(passengers[3].name);
 	}
 }
--- src/com/pluralsight/flight/Passenger.java
+++ src/com/pluralsight/flight/Passenger.java
@@ -1,14 +1,23 @@
 package com.pluralsight.flight;
 
-public final class Passenger {
+public final class Passenger implements Comparable {
     private int checkedBags;
     private int freeBags;
     private double perBagFee;
+    public int memberLevel; // 3(1st priority), 2, 1
+    public int memberDays;
+    public String name;
 
     public int getCheckedBags() { return this.checkedBags; }
     public void setCheckedBags(int checkedBags) { this.checkedBags = checkedBags; }
     public int getFreeBags() { return this.freeBags; }
     public void setFreeBags(int freeBags) { this.freeBags = freeBags; }
+
+    public void setLevelAndDays(int level, int days, String name) {
+        this.memberLevel = level;
+        this.memberDays = days;
+        this.name = name;
+    }
 
     public Passenger() {}
 
@@ -24,5 +33,23 @@ public final class Passenger {
 
     private Passenger(double perBagFee) {
         this.perBagFee = perBagFee;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Passenger p = (Passenger) o;
+        if (this.memberLevel > p.memberLevel) {
+            return -1;
+        } else if (p.memberLevel > this.memberLevel) {
+            return 1;
+        } else {
+            if (this.memberDays > p.memberDays) {
+                return -1;
+            } else if (p.memberDays > this.memberDays) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
     }
 }

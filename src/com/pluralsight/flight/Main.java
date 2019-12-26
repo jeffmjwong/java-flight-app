1369594e98f8ac70a9c36a2c12772138103fcdf8
Jeff Wong <v-jwong1@expedia.com>
2019-12-26 15:33:17 +1000

    create inner class RewardProgram in Passenger class

--- src/com/pluralsight/flight/Main.java
+++ src/com/pluralsight/flight/Main.java
@@ -1,38 +1,10 @@
 package com.pluralsight.flight;
 
-import static com.pluralsight.flight.Flight.resetAllPassengers;
-import static com.pluralsight.flight.Flight.getAllPassengers;
-
 public class Main {
 
     public static void main(String[] args) {
-    	Flight lax045 = new Flight(45);
-    	CrewMember pilot = new CrewMember("pilot");
-    	CrewMember copilot = new CrewMember("copilot");
-    	CrewMember marshal = new CrewMember("marshal");
-    	Passenger bob = new Passenger("bob");
-    	Passenger jane = new Passenger("jane");
-    	Passenger steve = new Passenger("steve");
-    	Passenger lisa = new Passenger("lisa");
-
-    	CrewMember[] crewMembers = { pilot, copilot, marshal };
-    	Passenger[] rosterMembers = { bob, jane, steve, lisa };
-
-    	lax045.setCrew(crewMembers);
-    	lax045.setRoster(rosterMembers);
-
-    	for (Person p : lax045) {
-    		System.out.println(p.getName());
-		}
-
-		lax045.add1Passenger();
-		lax045.add1Passenger();
-		lax045.add1Passenger();
-		Flight b = new Flight(90);
-		b.add1Passenger();
-		b.add1Passenger();
-    	System.out.println(getAllPassengers());
-    	resetAllPassengers();
-		System.out.println(getAllPassengers());
+    	Passenger steve = new Passenger();
+    	steve.getRewardProgram().setMemberLevel(3);
+    	System.out.println(steve.getRewardProgram().getMemberLevel());
 	}
 }
--- src/com/pluralsight/flight/Passenger.java
+++ src/com/pluralsight/flight/Passenger.java
@@ -4,18 +4,38 @@ public final class Passenger extends Person implements Comparable<Passenger> {
     private int checkedBags;
     private int freeBags;
     private double perBagFee;
-    private int memberLevel; // 3(1st priority), 2, 1
-    private int memberDays;
+
+    public static class RewardProgram {
+        private int memberLevel; // 3(1st priority), 2, 1
+        private int memberDays;
+
+        public int getMemberLevel() {
+            return this.memberLevel;
+        }
+
+        public void setMemberLevel(int memberLevel) {
+            this.memberLevel = memberLevel;
+        }
+
+        public int getMemberDays() {
+            return this.memberDays;
+        }
+
+        public void setMemberDays(int memberDays) {
+            this.memberDays = memberDays;
+        }
+    }
+
+    private RewardProgram rewardProgram = new RewardProgram();
+
+    public RewardProgram getRewardProgram() {
+        return this.rewardProgram;
+    }
 
     public int getCheckedBags() { return this.checkedBags; }
     public void setCheckedBags(int checkedBags) { this.checkedBags = checkedBags; }
     public int getFreeBags() { return this.freeBags; }
     public void setFreeBags(int freeBags) { this.freeBags = freeBags; }
-
-    public void setLevelAndDays(int level, int days) {
-        this.memberLevel = level;
-        this.memberDays = days;
-    }
 
     public Passenger() {}
 
@@ -38,14 +58,14 @@ public final class Passenger extends Person implements Comparable<Passenger> {
     }
 
     public int compareTo(Passenger p) {
-        if (this.memberLevel > p.memberLevel) {
+        if (this.getRewardProgram().getMemberLevel() > p.getRewardProgram().getMemberLevel()) {
             return -1;
-        } else if (p.memberLevel > this.memberLevel) {
+        } else if (p.getRewardProgram().getMemberLevel() > this.getRewardProgram().getMemberLevel()) {
             return 1;
         } else {
-            if (this.memberDays > p.memberDays) {
+            if (this.getRewardProgram().getMemberDays() > p.getRewardProgram().getMemberDays()) {
                 return -1;
-            } else if (p.memberDays > this.memberDays) {
+            } else if (p.getRewardProgram().getMemberDays() > this.getRewardProgram().getMemberDays()) {
                 return 1;
             } else {
                 return 0;
